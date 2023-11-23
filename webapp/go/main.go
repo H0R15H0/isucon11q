d20f0f07fd8512d69784890a0aaa673de4e3f2b3
H0R15H0 <shohori13@gmail.com>
2023-11-23 19:20:54 +0900

    環境変数でローカルのユーザを設定するようにした。

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -266,29 +266,32 @@ func getSession(r *http.Request) (*sessions.Session, error) {
 }
 
 func getUserIDFromSession(c echo.Context) (string, int, error) {
-	// session, err := getSession(c.Request())
-	// if err != nil {
-	// 	return "", http.StatusInternalServerError, fmt.Errorf("failed to get session: %v", err)
-	// }
-	// _jiaUserID, ok := session.Values["jia_user_id"]
-	// if !ok {
-	// 	return "", http.StatusUnauthorized, fmt.Errorf("no session")
-	// }
+	if os.Getenv("ISUCONDITION_DEBUG") == "1" {
+		return "isucon1", 0, nil
+	}
+	session, err := getSession(c.Request())
+	if err != nil {
+		return "", http.StatusInternalServerError, fmt.Errorf("failed to get session: %v", err)
+	}
+	_jiaUserID, ok := session.Values["jia_user_id"]
+	if !ok {
+		return "", http.StatusUnauthorized, fmt.Errorf("no session")
+	}
 
-	// jiaUserID := _jiaUserID.(string)
-	// var count int
+	jiaUserID := _jiaUserID.(string)
+	var count int
 
-	// err = db.Get(&count, "SELECT COUNT(*) FROM `user` WHERE `jia_user_id` = ?",
-	// 	jiaUserID)
-	// if err != nil {
-	// 	return "", http.StatusInternalServerError, fmt.Errorf("db error: %v", err)
-	// }
+	err = db.Get(&count, "SELECT COUNT(*) FROM `user` WHERE `jia_user_id` = ?",
+		jiaUserID)
+	if err != nil {
+		return "", http.StatusInternalServerError, fmt.Errorf("db error: %v", err)
+	}
 
-	// if count == 0 {
-	// 	return "", http.StatusUnauthorized, fmt.Errorf("not found: user")
-	// }
+	if count == 0 {
+		return "", http.StatusUnauthorized, fmt.Errorf("not found: user")
+	}
 
-	return "isucon1", 0, nil
+	return jiaUserID, 0, nil
 }
 
 func getJIAServiceURL(tx *sqlx.Tx) string {
